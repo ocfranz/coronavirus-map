98623778e1e869701cf26f6e612c1772ca3634e3
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-03-03 17:22:00 -0500

    Adding initial files

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="ocFranz";
+config.admin.email="franzjoseo@gmail.com";
+config.app.name="coronavirus-map";
+config.app.url="";
+
+module.exports=config
---
+++ src/app/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="page">
+        <div className="home">
+          <p>Welcome to the ReactJS and ExpressJS generator</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
