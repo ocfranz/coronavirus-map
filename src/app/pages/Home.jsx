aa446b83ca9e65301cb1b585af804eb26de3907d
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-03-03 18:05:16 -0500

    Adding global styles

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-
 import Button from '../components/Button';
+import {GlobalStyle} from '../styles/gobal'; 
 class Home extends React.Component {
   render() {
     return (
       <div className="page">
+        <GlobalStyle />
         <div className="home">
-          <Button> Button example</Button>
+          <Button> Button exaddmple</Button>
           <p>Welcome to the ReactJS and ExpressJS generator</p>
         </div>
       </div>
